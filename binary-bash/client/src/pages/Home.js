65fdd5811ce18a0b0b67cc084748deb343734266
Miguel <miguelvasquez2201@gmail.com>
2024-02-09 16:54:59 -0500

    added elements and css to homepage and sign up page

--- binary-bash/client/src/pages/Home.js
+++ binary-bash/client/src/pages/Home.js
@@ -1,15 +1,32 @@
 import {NavLink} from "react-router-dom";
+import '../styles/home.css'
 
 function Home() {
 
     return (
-        <div>
+        <div className="home-page">
             <h1>binaryBash</h1>
-            <a>Login</a>
-            <a>Signup</a>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="button-group">
+                <NavLink to='/login'><button>Login</button></NavLink>
+                <NavLink to='/signup'><button>Signup</button></NavLink>
+            </div>
+            
+            <div className="paragraph">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
 
-            <NavLink to="/login">Get started</NavLink>
+            <NavLink to="/login"><button className="get-started-btn">Get started</button></NavLink>
         </div>
       ); 
 };
--- binary-bash/client/src/pages/Signup.js
+++ binary-bash/client/src/pages/Signup.js
@@ -1,5 +1,24 @@
-function Signup() {
+import '../styles/signup.css'
+import { NavLink } from 'react-router-dom'
 
+function Signup() {
+  return (
+    <div className="signup-container">
+      <div className='form-container'>
+        <form>
+          <label htmlFor = 'username'>Username:</label>
+          <input type ='text' name='username' id='username-textfield'/>
+          <label htmlFor = 'password'>Password:</label>
+          <input type ='text' name='password' id='password-textfield'/>
+          <input type='submit' placeholder='Sign Up' id='submit-btn'/>
+        </form>
+
+        <label htmlFor = 'check'>By creating an account, you agree with our terms and conditions.</label>
+        <input type='checkbox' name='check'/>
+        <p>Already have an account? <NavLink to = '/login'>Login Here</NavLink></p>
+      </div>
+    </div>
+  )
 };
 
 export default Signup;
