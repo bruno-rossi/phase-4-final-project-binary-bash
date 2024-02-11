fe279ed32f9b7d85e805c944893c5ddb6ccab1aa
Miguel <miguelvasquez2201@gmail.com>
2024-02-10 22:56:01 -0500

    created python files and footer element

--- binary-bash/client/src/components/Footer.js
+++ binary-bash/client/src/components/Footer.js
@@ -1,5 +1,17 @@
-function Footer() {
+import '../styles/footer.css'
 
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <ul className='footer-list'>
+        <li>© 2024 binaryBash, Inc.</li>
+        <li>Terms</li>
+        <li>Privacy</li>
+        <li>Security</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
 };
 
 export default Footer;
