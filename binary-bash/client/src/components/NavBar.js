128e23a4efaf2e4eab8cb68c8208737784aa676b
Miguel <miguelvasquez2201@gmail.com>
2024-02-11 21:35:11 -0500

    edited navbar, post reqs for login, required fields for login/signup

--- binary-bash/client/src/components/NavBar.js
+++ binary-bash/client/src/components/NavBar.js
@@ -3,10 +3,17 @@ import '../styles/navbar.css'
 
 function NavBar() {
   return (
-    <div className='navbar-container'>
-      <NavLink to='/'><h1>binaryBash</h1></NavLink>
-      <NavLink to='/dashboard'><h3>myBashes</h3></NavLink>
-    </div>
+      <div className='navbar-container'>
+        <div className='left-nav'>
+          <NavLink to='/'><h1>binaryBash</h1></NavLink>
+          <NavLink to='/dashboard'><h3>myBashes</h3></NavLink>
+        </div>
+
+        <div className="button-group">
+          <NavLink to='/login'><button>Login</button></NavLink>
+          <NavLink to='/signup'><button>Signup</button></NavLink>
+        </div>
+      </div>
   )
 };
 
--- binary-bash/client/src/pages/Login.js
+++ binary-bash/client/src/pages/Login.js
@@ -1,16 +1,63 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from 'react'
 import '../styles/login.css'
 
 function Login() {
+  const [data, setData] = useState([])
+  const [username, setUsername] = useState('')
+  const navigate = useNavigate()
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ username })
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+      }
+      throw new Error ("username doesn't exists")
+    })
+    .then(user => {
+      setData([...data, user])
+      navigate('/dashboard')
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
   return (
     <div className="login-container">
       <div className='form-container'>
         <h1>Log In</h1>
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit}>
+
           <label htmlFor = 'username'>Username:</label>
-          <input type ='text' name='username' id='username-textfield' required/>
+          <input 
+          type ='text' 
+          name='username' 
+          id='username-textfield' 
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+          required
+          />
+
           <label htmlFor = 'password'>Password:</label>
-          <input type ='text' name='password' id='password-textfield' required/>
+          <input 
+          type ='text' 
+          name='password' 
+          id='password-textfield' 
+          required
+          />
+
           <input type='submit' placeholder='Sign Up' id='submit-btn'/>
         </form>
 
