1a389f2dae997c4480768931b81c5bb86910157a
Miguel <miguelvasquez2201@gmail.com>
2024-02-14 16:46:13 -0500

    added guest view for dash and error handling for passwords

--- binary-bash/client/src/components/EmptyCard.js
+++ binary-bash/client/src/components/EmptyCard.js
@@ -1,8 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 // import "../styles/EventCard.css";
 
 function EmptyCard() {
+  const { user, setUser } = useOutletContext();
 
+
+  if (!user) {
+    return (
+      <Link to="/login" className="empty-event-card">
+        <div>
+          <h1>Create New Event</h1>
+          <p className="empty-add-symbol">+</p>
+        </div>
+      </Link>
+    )
+  } else {
     return (
       <Link to="/create-event/" className="empty-event-card">
         <div>
@@ -11,6 +23,7 @@ function EmptyCard() {
         </div>
       </Link>
     )
+  }
 };
 
 export default EmptyCard;
